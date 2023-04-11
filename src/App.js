b0ac747ddee8c9d5f0be6a103190c03255a673ad
hS71Ru <harsh.sara45@gmail.com>
2023-04-12 01:31:49 +0530

    editing the list & adding animation

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
     this.handleInput=this.handleInput.bind(this);
     this.addItem=this.addItem.bind(this);
     this.deleteItem=this.deleteItem.bind(this);
+    this.setUpdate=this.setUpdate.bind(this);
 
   }
   handleInput(e){
@@ -51,6 +52,15 @@ class App extends Component {
     this.setState({items:filterItems,})
 
   }
+  setUpdate(text, key){
+    const items=this.state.items;
+    items.map(item => {
+      if(item.key===key){
+        item.text=text
+      }
+    })
+    this.setState({items:items,})
+  }
   render() {
     return (
     <div className="App">
@@ -61,7 +71,8 @@ class App extends Component {
       </form>
       </header>
       <ListItems items={this.state.items}
-      deleteItem={this.deleteItem}> </ListItems>
+      deleteItem={this.deleteItem} 
+      setUpdate={this.setUpdate}> </ListItems>
     </div>
     )
   }
